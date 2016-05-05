500024f077772f4eb537505de3ead22f4e5871fc
benjamam <benjamam@miamioh.edu>
2016-05-05 16:37:51 -0400

    created functions

--- Dev/Dev.ino
+++ Dev/Dev.ino
@@ -16,7 +16,7 @@ int incrTime = 0;
 int decrTime = 0;
 
 //alarm time and date
-int alarmHour = 20;
+int alarmHour = 14;
 int alarmMinute = 45;
 
 //LED initialization
@@ -26,22 +26,24 @@ int sunrisespeed = 4500;
 int i;
 int j;
 int pulsepin = 2;
-/*
+
 int delays[64] = {1,2,4,6,9,12,16,20,25,30,36,
 42,49,56,64,72,81,90,100,110,121,132,
 144,156,169,182,196,210,225,240,256,272,289,
 306,324,342,361,380,400,420,441,462,484,506,
 529,552,576,600,625,650,676,702,729,756,784,
-812,841,870,900,930,961,992,992,992}; */
+812,841,870,900,930,961,992,992,992}; 
 
+/*
 int delays[64] = {1,2,4,6,9,12,16,20,25,30,36,
 42,49,56,64,72,81,90,100,110,121,132,
 144,156,169,182,196,210,240,260,280,300,320,
 340,360,380,400,420,440,465,490,515,540,565,
 590,615,640,665,695,697,701,702,729,756,784,
 812,841,870,900,930,961,992,992,992};
+*/
 
-// initialize the LCD library with the numbers of the interface pins
+// initialize the LCD library
 LiquidCrystal lcd(7,8,9,10,11,12);
 
 //initialize the real time clock
@@ -60,6 +62,47 @@ void displayCurrentTime(DateTime now){
     lcd.print(now.minute());  
 }
 
+void incrementAlarmTime(){
+  alarmMinute = alarmMinute + 1;
+  delay(100); //debounce
+  if(alarmMinute >= 60){
+     alarmMinute = 0;
+     alarmHour = alarmHour + 1;
+     if(alarmHour > 23){
+       alarmHour = 0;
+     }
+   }
+}
+
+void decrementAlarmTime(){
+  alarmMinute = alarmMinute - 1;
+  delay(100); //debounce
+  if(alarmMinute < 0){
+     alarmMinute = 59;
+     alarmHour = alarmHour - 1;
+     if(alarmHour < 0){
+       alarmHour = 23;
+     }
+   }
+}
+
+void turnOnLED(DateTime now){
+  for (i=0; i<steps; i++){
+        pwmTime = delays[i] * 5;
+        for (j=0; j<sunrisespeed; j++)
+        {
+          // one pulse of PWM
+          digitalWrite(pulsepin, HIGH);
+          delayMicroseconds(pwmTime);
+          digitalWrite(pulsepin, LOW);
+          delayMicroseconds(5000-pwmTime);
+
+          now = rtc.now();
+          displayCurrentTime(now);
+        }
+      }
+}
+
 void setup () {
 
   pinMode(incrTimePin, INPUT);
@@ -68,7 +111,7 @@ void setup () {
   pinMode(pulsepin, OUTPUT);
   
   lcd.begin(16, 2);
-  // Print a message to the LCD.
+  
   lcd.print("Alarm:");
   lcd.setCursor(0,1);
   lcd.print("Time:");
@@ -112,66 +155,25 @@ void loop () {
     displayCurrentTime(now);
 
     /*Logic for alarm to be triggered*/
-    if(alarmHour == now.hour() && (alarmMinute-now.minute()) = 15){ //removed <=
+    if((alarmHour == now.hour()) && ((alarmMinute-now.minute()) == 15)){ 
       Serial.println("Alarm on");
-      for (i=0; i<steps; i++){
-        pwmTime = delays[i] * 5;
-        for (j=0; j<sunrisespeed; j++)
-        {
-          // one pulse of PWM
-          digitalWrite(pulsepin, HIGH);
-          delayMicroseconds(pwmTime);
-          digitalWrite(pulsepin, LOW);
-          delayMicroseconds(5000-pwmTime);
-
-          now = rtc.now();
-          displayCurrentTime(now);
-        }
-      }
+      turnOnLED(now);
     }//end if
-    else if((alarmHour-now.hour()) == 1 && (now.minute()-alarmMinute) = 45){//removed <=
+    else if(((alarmHour-now.hour()) == 1) && ((now.minute()-alarmMinute) == 45)){  
       Serial.println("Alarm on");
-      for (i=0; i<steps; i++){
-        pwmTime = delays[i] * 5;
-        for (j=0; j<sunrisespeed; j++){
-          // one pulse of PWM
-          digitalWrite(pulsepin, HIGH);
-          delayMicroseconds(pwmTime);
-          digitalWrite(pulsepin, LOW);
-          delayMicroseconds(5000-pwmTime);
-
-          now = rtc.now();
-          displayCurrentTime(now);
-        }
-      }
+      turnOnLED(now);
     }//end else if
 
     //logic to increase alarm time
     incrTime = digitalRead(incrTimePin);
     if(incrTime == 0){
-      alarmMinute = alarmMinute + 1;
-      delay(100); //debounce
-      if(alarmMinute >= 60){
-        alarmMinute = 0;
-        alarmHour = alarmHour + 1;
-        if(alarmHour > 23){
-          alarmHour = 0;
-        }
-      }
+      incrementAlarmTime();
     }
 
     //logic to decrease alarm time
     decrTime = digitalRead(decrTimePin);
     if(decrTime == 0){
-      alarmMinute = alarmMinute - 1;
-      delay(100); //debounce
-      if(alarmMinute < 0){
-        alarmMinute = 59;
-        alarmHour = alarmHour - 1;
-        if(alarmHour < 0){
-          alarmHour = 23;
-        }
-      }
+      decrementAlarmTime();
     }
     
     delay(100);
