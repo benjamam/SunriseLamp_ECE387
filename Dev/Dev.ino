c0f19d61da3148973e4f2dc1364b9b673f0d6e4b
benjamam <benjamam@miamioh.edu>
2016-05-05 11:11:58 -0400

    photo upload attempt

--- Dev/Dev.ino
+++ Dev/Dev.ino
@@ -16,22 +16,29 @@ int incrTime = 0;
 int decrTime = 0;
 
 //alarm time and date
-int alarmHour = 23;
+int alarmHour = 20;
 int alarmMinute = 45;
 
 //LED initialization
-int duty = 0;
+int pwmTime = 0;
 int steps = 64;
-int sunrisespeed = 5000;
+int sunrisespeed = 4500;
 int i;
 int j;
 int pulsepin = 2;
-
-int lookup[64] = {1,2,4,6,9,12,16,20,25,30,36,
+/*
+int delays[64] = {1,2,4,6,9,12,16,20,25,30,36,
 42,49,56,64,72,81,90,100,110,121,132,
 144,156,169,182,196,210,225,240,256,272,289,
 306,324,342,361,380,400,420,441,462,484,506,
 529,552,576,600,625,650,676,702,729,756,784,
+812,841,870,900,930,961,992,992,992}; */
+
+int delays[64] = {1,2,4,6,9,12,16,20,25,30,36,
+42,49,56,64,72,81,90,100,110,121,132,
+144,156,169,182,196,210,240,260,280,300,320,
+340,360,380,400,420,440,465,490,515,540,565,
+590,615,640,665,695,697,701,702,729,756,784,
 812,841,870,900,930,961,992,992,992};
 
 // initialize the LCD library with the numbers of the interface pins
@@ -105,31 +112,35 @@ void loop () {
     displayCurrentTime(now);
 
     /*Logic for alarm to be triggered*/
-    if(alarmHour == now.hour() && (alarmMinute-now.minute()) <= 15){
+    if(alarmHour == now.hour() && (alarmMinute-now.minute()) = 15){ //removed <=
       Serial.println("Alarm on");
       for (i=0; i<steps; i++){
-        duty = lookup[i] * 5;
+        pwmTime = delays[i] * 5;
         for (j=0; j<sunrisespeed; j++)
         {
           // one pulse of PWM
           digitalWrite(pulsepin, HIGH);
-          delayMicroseconds(duty);
+          delayMicroseconds(pwmTime);
           digitalWrite(pulsepin, LOW);
-          delayMicroseconds(5000-duty);
+          delayMicroseconds(5000-pwmTime);
+
+          now = rtc.now();
           displayCurrentTime(now);
         }
       }
     }//end if
-    else if((alarmHour-now.hour()) == 1 && (now.minute()-alarmMinute) >= 45){
+    else if((alarmHour-now.hour()) == 1 && (now.minute()-alarmMinute) = 45){//removed <=
       Serial.println("Alarm on");
       for (i=0; i<steps; i++){
-        duty = lookup[i] * 5;
+        pwmTime = delays[i] * 5;
         for (j=0; j<sunrisespeed; j++){
           // one pulse of PWM
           digitalWrite(pulsepin, HIGH);
-          delayMicroseconds(duty);
+          delayMicroseconds(pwmTime);
           digitalWrite(pulsepin, LOW);
-          delayMicroseconds(5000-duty);
+          delayMicroseconds(5000-pwmTime);
+
+          now = rtc.now();
           displayCurrentTime(now);
         }
       }
@@ -139,6 +150,7 @@ void loop () {
     incrTime = digitalRead(incrTimePin);
     if(incrTime == 0){
       alarmMinute = alarmMinute + 1;
+      delay(100); //debounce
       if(alarmMinute >= 60){
         alarmMinute = 0;
         alarmHour = alarmHour + 1;
@@ -152,6 +164,7 @@ void loop () {
     decrTime = digitalRead(decrTimePin);
     if(decrTime == 0){
       alarmMinute = alarmMinute - 1;
+      delay(100); //debounce
       if(alarmMinute < 0){
         alarmMinute = 59;
         alarmHour = alarmHour - 1;
